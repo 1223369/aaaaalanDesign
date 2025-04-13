23346b9d59988aa406205f9561852e56d12ea30e
v-chenx83 <1614819989@qq.com>
2025-04-13 22:54:52 +0800

    update:完善初始化项目

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import './style.less'
+import 'virtual:svg-icons-register'
 
 createApp(App).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,58 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import AutoImport from "unplugin-auto-import/vite";
+import vue from "@vitejs/plugin-vue";
+import { ArcoResolver } from "unplugin-vue-components/resolvers";
+import Components from "unplugin-vue-components/vite";
+import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
+import { resolve } from "path";
+
+const config = ({ mode }) => {
+  return {
+    plugin: [
+      vue(),
+      // 自动按需引入组件
+      AutoImport({
+        resolvers: [
+          ArcoResolver({
+            // importStyle: 'less',
+          }),
+        ],
+        imports: ["vue", "vue-router", "pinia", "@vueuse/core"],
+        eslintrc: {
+          enabled: true,
+        },
+      }),
+      Components({
+        directoryAsNamespace: true,
+        // 自定义组件的自动导入目录
+        dirs: [
+          "src/components",
+          "src/views/components", // 如果views 中也有可复用的组件
+        ],
+        // 组件的有效文件扩展名
+        extensions: ["vue", "tsx"],
+        resolvers: [
+          // 自动引入arco
+          ArcoResolver({
+            // importStyle: 'less',
+            resolveIcons: true,
+          }),
+        ],
+      }),
+      createSvgIconsPlugin({
+        // 指定需要缓存的图标文件夹
+        iconDirs: [resolve(process.cwd(), "src/assets/icons")],
+        // 指定symbolId格式
+        symbolId: "icon-[dir]-[name]",
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src"),
+      },
+    },
+  };
+};
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default defineConfig(config);
