a0dcfb4b31ad35da47bae95ccbc8621b53436a49
v-chenx83 <1614819989@qq.com>
2026-01-05 00:20:08 +0800

    修复缺陷

--- src/views/Editor/layouts/header/left/operation.vue
+++ src/views/Editor/layouts/header/left/operation.vue
@@ -1,13 +1,18 @@
 <template>
     <a-space>
-        <DropdownButton v-for="(button, index) in editToolList" :key="button.key" :active="activeTool === button.key"
-            @click="onClick(button.key)" @select="
-                (value) => {
-                    onSelect(index, value);
-                }
-            ">
-            <SvgIcon :name="button.icon" />
-             <template #content v-if="button.doption">
+        <DropdownButton
+                v-for="(button, index) in editToolList"
+                :key="button.key"
+                :active="activeTool === button.key"
+                @click="onClick(button.key)"
+                @select="
+        (value: any) => {
+          onSelect(index,value)
+        }
+      "
+        >
+            <SvgIcon :name="button.icon"/>
+            <template #content v-if="button.doption">
                 <a-doption v-for="doption in button.doption" :key="doption.key" :value="doption">
                     <template #icon>
                         <SvgIcon :name="doption.icon"/>
@@ -21,54 +26,80 @@
 
 <script setup lang="ts">
 import SvgIcon from '@/components/svgIcon'
-import DropdownButton from "@/components/dropdown/dropdown.vue";
-import type { EditTool } from "app";
-import { useAppStore } from "@/store/index";
+import DropdownButton from '@/components/dropdown/dropdown.vue'
+import {useAppStore} from '@/store'
+import type {EditTool} from 'app'
+// import {useEditor} from '@/views/Edito'
 
-const { activeTool } = storeToRefs(useAppStore());
+// const {canvas, undoRedo} = useEditor()
+
+const {activeTool} = storeToRefs(useAppStore())
 
 type EditToolListItem = {
-    key: EditTool;
-    icon: string;
-    name: string;
-};
+    key: EditTool
+    icon: string
+    name: string
+}
 
 type EditToolList = (EditToolListItem & {
-    doption?: EditToolListItem[];
-})[];
+    doption?: EditToolListItem[]
+})[]
 
-const editToolList = ref<EditToolList>(<
-    (EditToolListItem & { doption?: EditToolListItem[] })[]
-    >[
-        {
-            key: "select",
-            icon: "bxs-pointer",
-            name: "选择工具",
-            doption: [
-                {
-                    key: "select",
-                    icon: "bxs-pointer",
-                    name: "选择工具",
-                },
-                {
-                    key: "handMove",
-                    icon: "bxs-hand",
-                    name: "移动视图",
-                },
-            ],
-        },
-        {
-            key: "pen",
-            icon: "bx-pen",
-            name: "钢笔",
-        },
-    ]);
+const editToolList = ref<EditToolList>(<(EditToolListItem & { doption?: EditToolListItem[] })[]>[
+    {
+        key: 'select',
+        icon: 'bxs-pointer',
+        name: '选择工具',
+        doption: [
+            {
+                key: 'select',
+                icon: 'bxs-pointer',
+                name: '选择工具',
+            },
+            {
+                key: 'handMove',
+                icon: 'bxs-hand',
+                name: '移动视图',
+            },
+        ],
+    },
+    {
+      key: 'pen',
+      icon: 'bx-pen',
+      name: '钢笔',
+    },
+])
+// 监听activeTool
+watch(activeTool, (newTool, oldTool) => {
+    const actArr = getFilteredDataByKey(newTool)
+    if (actArr.length>0){
+        const value = actArr[0]
+        onSelect(value.index,value.data)
+    }
+})
+function getFilteredDataByKey(key: string) {
+    const result: { data: EditToolListItem | undefined, index: number }[] = [];
 
-const onClick = (key: EditTool) => {
-};
+    // 查找editToolList中满足key条件的数据
+    editToolList.value.forEach((item, index) => {
+        if (item.key === key) {
+            result.push({ data: item, index });
+        }
+        if (item.doption) {
+            const subItemIndex = item.doption.findIndex(subItem => subItem.key === key);
+            if (subItemIndex !== -1) {
+                result.push({ data: item.doption[subItemIndex], index });
+            }
+        }
+    });
 
-const onSelect = (index: number, value: any) => {
-    console.log("index", index);
-    console.log("value", value);
-};
+    return result;
+}
+const onSelect = (index:number,value:any) => {
+    editToolList.value[index] = { ...editToolList.value[index], ...value as EditToolListItem }
+    onClick((value as EditToolListItem).key)
+}
+const onClick = (toolName: EditTool) => {
+    activeTool.value = toolName
+}
 </script>
